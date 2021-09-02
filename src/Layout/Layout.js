7c051493962f619a04c8fa8d046e7894b10f3d4e
Rudi <rudix.lab@gmail.com>
2021-09-02 11:03:12 +0300

    ddd

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -6,17 +6,41 @@ const Layout = ({ children, title, active, user }) => {
         <Menu.Item>
           <a href='https://vicove.netlify.app'> 😜 ВИЦОВЕ </a>
         </Menu.Item>
-        <Menu.Item key='register'>
-          <a href='/app/register'>Печели пари (Регистация)</a>
-        </Menu.Item>
-        <Menu.Item key='login'>
-          <a href='/app/login'>Вход</a>
-        </Menu.Item>
-        <Menu.Item key='forgot'>
-          <a href='/app/forgot'>Забравена Парола</a>
-        </Menu.Item>
+        {!user ? (
+          <>
+            <Menu.Item key='register'>
+              <a href='/app/register'>Печели пари (Регистация)</a>
+            </Menu.Item>
+            <Menu.Item key='login'>
+              <a href='/app/login'>Вход</a>
+            </Menu.Item>
+            <Menu.Item key='forgot'>
+              <a href='/app/forgot'>Забравена Парола</a>
+            </Menu.Item>
+          </>
+        ) : (
+          <>
+            <Menu.Item>
+              <a href='https://vicove.netlify.app'> 😜 ВИЦОВЕ </a>
+            </Menu.Item>
+            <Menu.Item key='register'>
+              <a href='/app/register'>Печели пари (Регистация)</a>
+            </Menu.Item>
+            <Menu.Item key='login'>
+              <a href='/app/login'>Вход</a>
+            </Menu.Item>
+            <Menu.Item key='forgot'>
+              <a href='/app/forgot'>Забравена Парола</a>
+            </Menu.Item>
+          </>
+        )}
       </Menu>
-      <Row type='flex' justify='center' align='top' style={{ marginTop: 20 }}>
+      <Row
+        type='flex'
+        justify='center'
+        align='top'
+        style={{ marginTop: 20, minHeight: 500 }}
+      >
         <Col xs={23} sm={20} md={16} lg={15} xl={12}>
           {title && <h1>{title}</h1>}
           {children}
